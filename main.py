5acbb0315e1e754647ac853b979289c29b6d59eb
Sneha Kapoor <sneha.kapoor@somaiya.edu>
2024-01-29 17:17:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import feedparser
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base
 from datetime import datetime
 
 # Define the SQLite database engine
